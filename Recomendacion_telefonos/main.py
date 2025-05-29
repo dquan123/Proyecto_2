69b5311d097b545a63401b70ffb887a9906d98d6
Gonza <gon24170@uvg.edu.gt>
2025-05-29 11:20:14 -0600

    login y verificar_login agregados al main.

--- Recomendacion_telefonos/main.py
+++ Recomendacion_telefonos/main.py
@@ -1,10 +1,40 @@
 import sys
 import os
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+
+# carga de informacion dotenv
+load_dotenv()
+
+# ingreso de credenciales de la base de datos.
+uri = input("URI de Neo4j: ")
+user_db = input("Usuario de Neo4j: ")
+password_db = input("Contraseña: ")
+
+driver = GraphDatabase.driver(uri, auth=(user_db, password_db))
 
 # Agrega la raíz del proyecto al sys.path
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 from app.services.recommendation_service import recommend_phones_for_user
+def verificar_login(tx, id, usuario):
+    query = (
+
+    )
+
+    result = tx.run(query, usuario=usuario, id = id)
+    record = result.single()
+    return record["usuario"] if record else None
+
+def login(usuario, id):
+    with driver.session() as session:
+        result = session.read_transaction(verificar_login, usuario, id)
+        if result:
+            print(f"Bienvenido, {result}!\n")
+            return True
+        else:
+            print("Usuario o contraseña incorrectos.\n")
+            return False
 
 def main():
     print("Bienvenido al Sistema de Recomendación de Teléfonos 📱")
@@ -24,3 +54,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    driver.close()
